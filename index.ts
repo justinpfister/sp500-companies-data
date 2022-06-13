29da0c25a6a60a451da7759d3e8669141064bf70
Justin Pfister <justinpfister@users.noreply.github.com>
2022-06-13 12:50:31 -0400

    sp500 CSV updater

--- index.ts
+++ index.ts
@@ -1,24 +1,57 @@
 import axios from 'axios';
-import jsdom from 'jsdom';
-
-let sayhi = async () => {
-    const t = await getsp500();
-    console.log(t);
-}
-
-
-
+import cheerio from 'cheerio';
+import ObjectsToCsv from 'objects-to-csv';
+import path from 'path';
 
 let getsp500 = async () => {
 
     // https://en.wikipedia.org/wiki/List_of_S%26P_500_companies
     const response = await axios.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies",{ responseType: 'blob',});
-    const file = response.data;
+    const file = response;
 
-console.log(file);
+    const $ = cheerio.load(file.data);
 
+    const companies = $("#constituents > tbody > tr").map( (index,element) => {
+        //console.log($(element).html());
+
+        let ticker = $($(element).find("td")[0]).text().replace(/\n/g,'');
+        let security = $($(element).find("td")[1]).text();
+        let sector = $($(element).find("td")[3]).text();
+        let industry = $($(element).find("td")[4]).text();
+        let hq = $($(element).find("td")[5]).text();
+        let date_first_added = $($(element).find("td")[6]).text();
+        let cik = $($(element).find("td")[7]).text();
+        let founded = $($(element).find("td")[8]).text().replace(/\n/g,'');
+
+
+        let vars = {
+            ticker:ticker,
+            security:security,
+            sector:sector,
+            industry:industry,
+            hq:hq,
+            date_first_added:date_first_added,
+            cik:cik,
+            founded:founded
+        };
+
+        return vars;
+
+    }).toArray();
+
+    const csv = new ObjectsToCsv(companies);
+    // Save to file:
+    await csv.toDisk(__dirname + '/data/companies.csv');
+
+    // Return the CSV file as string:
+    console.log(await csv.toString());
+    console.log("updated company list");
+    
+    return;
+
+    
     
 }
 
 
-sayhi();
+getsp500();
